3bb650f6f35d056b67fa5024f72fa7b3e23f1181
kailyncleere <79721722+kailyncleere@users.noreply.github.com>
2025-03-12 15:22:17 -0500

    remove + check list items

--- Project-3-submission/js/index.js
+++ Project-3-submission/js/index.js
@@ -16,3 +16,11 @@ function AddTask() {
     TaskBox.value = '';
 }
 
+TaskList.addEventListener('click', function(e) {
+    if(e.target.tagName === 'LI') {
+        e.target.classList.toggle('checked');
+    }
+    else if(e.target.tagName === 'SPAN') {
+        e.target.parentElement.remove();
+    }
+}, false);
