2c2c514b1cc9bd710ba999e2b723c5bfe1da0b63
kailyncleere <79721722+kailyncleere@users.noreply.github.com>
2025-03-21 13:54:19 -0500

    display form setup tasks saving to local storage

--- Project-3-submission/React-web-app/todo-app/src/components/ToDoList.js
+++ Project-3-submission/React-web-app/todo-app/src/components/ToDoList.js
@@ -5,21 +5,43 @@ function ToDoList() {
     const [todos, setTodos] = useState([]);
 
     const AddToDo = todo => {
-        if(!todo.text || /^\s*$/.test(todo.text)) {
-            return
+        const trimmedText = todo.text.trim();
+
+        if (!trimmedText || /^\s*$/.test(trimmedText)) {
+            return;
         }
 
-        const newTodos = [todo, ...todos];
+        const newTodos = [{ ...todo, text: trimmedText }, ...todos];
 
         setTodos(newTodos);
-    }
-    console.log(...todos);
+        console.log('New task added:', newTodos);
+    };
 
+    // Marks a task as complete or incomplete
+    const completeTodo = id => {
+        let updatedTodos = todos.map(todo => {
+            if (todo.id === id) {
+                todo.isComplete = !todo.isComplete;
+            }
+            return todo;
+        });
+        setTodos(updatedTodos);
+        console.log('Task completed:', updatedTodos);
+    };
+
+    // Actual display of the to-do list
     return (
         <div>
             <h1>To-Do List</h1>
-            <ToDoForm  />
+            <ToDoForm onSubmit={AddToDo} />
+            <div>
+                <button onClick={() => setFilter('all')}>All</button>
+                <button onClick={() => setFilter('completed')}>Complete</button>
+                <button onClick={() => setFilter('uncompleted')}>Incomplete</button>
+            </div>
+            <ToDo todos={filteredTodos} completeTodo={completeTodo} removeTodo={removeTodo} updateTodo={updateTodo} />
         </div>
-    )
+    );
 }
-export default ToDoList
+
+export default ToDoList;
