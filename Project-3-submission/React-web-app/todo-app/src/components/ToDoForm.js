e0bb2b5e84ddfac3e297b7d095933eadd229c8a8
kailyncleere <79721722+kailyncleere@users.noreply.github.com>
2025-03-21 13:51:15 -0500

    set up todo form

--- Project-3-submission/React-web-app/todo-app/src/components/ToDoForm.js
+++ Project-3-submission/React-web-app/todo-app/src/components/ToDoForm.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
     function ToDoForm(props) {
         const [input, setInput] = useState('');
